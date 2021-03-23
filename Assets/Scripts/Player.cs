be549c05c5f94a78bd7b1915b0943b287cfc1424
BuildTools <unconfigured@null.spigotmc.org>
2021-03-23 18:36:10 +0000

    main

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+
+    float Horizontal = Input.GetAxis("Horizontal");
+    float Vertical = Input.GetAxis("Vertical");
+
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 movement = new Vector3(Horizontal, Vertical, 0)
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+    }
+}
